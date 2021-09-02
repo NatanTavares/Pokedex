fa80a5a24160dce4a2b1972493014fadfeed1f0c
NatanTavares <nattantavares.s15@gmail.com>
2021-09-02 16:51:30 -0300

    feat: Implements the components to build the home page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { Pagination } from "./components/Pagination";
 import { Header } from "./components/Header";
+import { Grid } from "./components/Grid";
+
+import { Container } from "./styles/home";
 
 function App() {
   return (
-    <main className="App">
+    <Container>
       <Header />
-    </main>
+      <Grid />
+      <Pagination
+        totalCountOfRegisters={50}
+        currentPage={3}
+        onPageChange={() => {}}
+      />
+    </Container>
   );
 }
 
---
+++ src/styles/home.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  footer {
+    margin-top: auto;
+  }
+`;
+
+export { Container };
